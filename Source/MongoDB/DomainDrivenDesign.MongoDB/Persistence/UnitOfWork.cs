67c04e96f072bd75eb6315a6fd11b853e3d283ff
Peter Morris <mrpmorris@gmail.com>
2021-01-30 20:25:20 +0000

    ValidationService

--- Source/MongoDB/DomainDrivenDesign.MongoDB/Persistence/UnitOfWork.cs
+++ Source/MongoDB/DomainDrivenDesign.MongoDB/Persistence/UnitOfWork.cs
@@ -1,23 +1,47 @@
-﻿using System.Threading.Tasks;
+﻿using DomainDrivenDesign.MongoDB.DomainClasses;
+using DomainDrivenDesign.MongoDB.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DomainDrivenDesign.MongoDB.Persistence
 {
 	public interface IUnitOfWork<TDbContext>
 		where TDbContext: DbContext
 	{
-		Task SaveChangesAsync();
+		Task<ValidationError[]> SaveChangesAsync();
 	}
 
 	public class UnitOfWork<TDbContext> : IUnitOfWork<TDbContext>
 		where TDbContext: DbContext
 	{
-		protected readonly TDbContext DbContext;
+		private readonly TDbContext DbContext;
+		private readonly IValidationService ValidationService;
 
-		public UnitOfWork(TDbContext dbContext)
+		public UnitOfWork(TDbContext dbContext, IValidationService validationService)
 		{
 			DbContext = dbContext;
+			ValidationService = validationService;
 		}
 
-		public Task SaveChangesAsync() => DbContext.SaveChangesAsync();
+		public async Task<ValidationError[]> SaveChangesAsync()
+		{
+			IEnumerable<AggregateRoot> entities = DbContext.GetEntries()
+				.Where(x =>
+					x.State == EntityState.Created
+					|| x.State == EntityState.Modified)
+				.Select(x => x.Entity);
+
+			ValidationError[] validationErrors = await ValidationService
+				.ValidateAsync(entities)
+				.ConfigureAwait(false);
+
+			if (validationErrors.Length > 0)
+				return validationErrors;
+
+			await DbContext.SaveChangesAsync().ConfigureAwait(false);
+			return Array.Empty<ValidationError>();
+		}
 	}
 }
